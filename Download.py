179ad20f29efbe3a88d743163fa2b8ff4869b811
Anis El Benna <benna.anis2@gmail.com>
2019-06-09 18:51:15 +0100

    First Commit

---
+++ Download.py
@@ -0,0 +1,34 @@
+import requests
+import shutil
+import os
+from pathlib import Path
+
+def CreatePath():
+    try:
+        if not os.path.exists("./Pics/"):
+            os.makedirs("Pics/")
+        else:
+            print("Pics folder already exists.")
+    except OSError:
+        print("Error")
+
+
+def Download(ch="10000001.jpg"):
+    basepath = Path('Pics/')
+    ph = basepath / ch
+    if ph.is_file():
+        pass
+    else:
+        while True:
+            try:
+                r = requests.get("https://ygoprodeck.com/pics/"+ch, timeout=5, stream=True, headers={'User-agent': 'Mozilla/5.0'}) #to allow(else 403 forbidden)
+                break
+            except Exception as ex:
+                print(ex)
+                print("EXCEPTION CAUGHT HERE !")
+        with open(ph, 'wb') as f: #copying data to file
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+            size = f.tell()
+            f.close()
+        return(size)
